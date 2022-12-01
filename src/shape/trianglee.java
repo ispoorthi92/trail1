dc74210c2965ffae4ef16335dad831059bc74259
ispoorthi92 <ispoorthi92@gmail.com>
2022-12-01 21:28:58 +0530

    commit dec1 2022

---
+++ src/shape/trianglee.java
@@ -0,0 +1,13 @@
+package shape;
+
+public class trianglee {
+static int a=3;
+ static int b=6;
+ 
+	public static void main(String[] args) {
+	System.out.println(a);
+ System.out.println(b);
+ 
+	}
+
+}
