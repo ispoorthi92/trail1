15f3a27e87c47a263c5ee2a61d4b522e3c6c6e99
ispoorthi92 <ispoorthi92@gmail.com>
2022-12-03 21:31:35 +0530

    03 dec 2022

---
+++ src/tree/mango.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class mango {
+static	int a=2;
+ static	int b=3;
+	
+
+	public static void main(String[] args) {
+		System.out.println( a);
+		System.out.println(b);
+
+	}
+
+}
